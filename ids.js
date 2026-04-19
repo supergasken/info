8e723f23f671524f76691054c78a8b00b3de6382
supergasken <59183113+supergasken@users.noreply.github.com>
2026-04-19 19:39:37 +0700

    Create ids.js

---
+++ ids.js
@@ -0,0 +1,9 @@
+const daftarID = {
+    "p1": "8ASEJhFgbD",
+    "p2": "1VvKNT9Woy",
+    "p3": "1BITysMUba",
+    "p4": "LjMzMZIXJ",
+    "p5": "9AKlVbw6vk",
+    "p6": "KODE_BARU_1", // Tinggal tambah di bawahnya kalau ada lagi
+    "p7": "KODE_BARU_2"
+};
